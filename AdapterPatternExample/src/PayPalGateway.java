75f82fc8c16ed6180c3aa3680a7281cfe6c11234
SUCHARITHA0820 <sucharithaammineni8@gmail.com>
2026-07-12 22:04:04 +0530

    Delete AdapterPatternExample directory

--- AdapterPatternExample/src/PayPalGateway.java
+++
@@ -1,5 +0,0 @@
-public class PayPalGateway {
-    public void sendPayment(double totalAmount) {
-        System.out.println("Payment of $" + totalAmount + " processed via PayPal.");
-    }
-}
--- AdapterPatternExample/src/StripeGateway.java
+++
@@ -1,5 +0,0 @@
-public class StripeGateway {
-    public void makeCharge(double amountInDollars) {
-        System.out.println("Payment of $" + amountInDollars + " processed via Stripe.");
-    }
-}
